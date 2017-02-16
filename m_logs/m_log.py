3bc4aa759ff6775072b152f8b9eccaf2ae71e30f
526399002 <526399002@qq.com>
2017-02-16 19:33:42 +0800

    edit: 编写django web中

--- m_logs/m_log.py
+++ m_logs/m_log.py
@@ -1,1 +1,3 @@
 import logging
+
+logging.basicConfig()
--- master/m_web/monitor/models.py
+++ master/m_web/monitor/models.py
@@ -8,13 +8,34 @@ class Schema(models.Model):
     
 
 class Field(models.Model):
+    INT = 0
+    FLOAT = 1
+    VARCHAR = 2
+    BOOL = 3
+    DATETIME = 4
+    FIELD_CHOICES = (
+        (INT, 'INT'),
+        (FLOAT, 'FLOAT'),
+        (VARCHAR, 'VARCHAR'),
+        (BOOL, 'BOOL'),
+        (DATETIME, 'DATETIME')
+    )
     schema = models.ForeignKey(Schema)
     name = models.CharField(max_length=45)
-    field_type = models.IntegerField()
+    display = models.CharField(max_length=100)
+    field_type = models.IntegerField(choices=FIELD_CHOICES)
+    required = models.BooleanField(default=False)
+    multiple = models.BooleanField(default=False)
+    unique = models.BooleanField(default=False)
+    default = models.CharField(max_length=200)
+    deleted = models.BooleanField(default=False)
     
 
-class Dead_Setting(models.Model):
-    
+class Entity(models.Model):
+    schema = models.ForeignKey(Schema)
 
-class Log_Setting(models.Model):
-    project = models.CharField(max_length=50)
+
+class Value(models.Model):
+    entity = models.ForeignKey(Entity)
+    field = models.ForeignKey(Field)
+    value = models.TextField()
