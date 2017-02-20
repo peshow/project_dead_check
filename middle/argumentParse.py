914cbf40c5e22d4431930a298df44663608187af
526399002 <526399002@qq.com>
2017-02-20 16:16:26 +0800

    规范文字性说明

--- middle/argumentParse.py
+++ middle/argumentParse.py
@@ -3,7 +3,7 @@ import argparse
 
 def arg_parse(arg=sys.argv[1:]):
     parse = argparse.ArgumentParser(description="自定义监控")
-    parse.add_argument("-d", dest="dead", action="store_true", default=False, help=":启动假死监控")
+    parse.add_argument("-d", dest="dead", action="store_true", default=False, help=":启动进程假死监控")
     parse.add_argument("-l", dest="log", action="store_true", default=False, help=":启动日志异常检索")
     rest = parse.parse_args(arg)
     return rest
